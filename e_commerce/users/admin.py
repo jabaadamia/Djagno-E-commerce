7d15b94bb67f7dc6cfee463c13b7d39abf5c6441
jaba adamia <jabaadamia@jabas-MacBook-Air.local>
2025-02-05 18:17:46 +0400

    added models to admin

--- e_commerce/users/admin.py
+++ e_commerce/users/admin.py
@@ -1,4 +1,4 @@
-from allauth.account.decorators import secure_admin_login
+from allauth.account.decorators import secure_admin_login  # noqa: I001
 from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
@@ -6,7 +6,7 @@ from django.utils.translation import gettext_lazy as _
 
 from .forms import UserAdminChangeForm
 from .forms import UserAdminCreationForm
-from .models import User
+from .models import User, Customer, Seller, Address
 
 if settings.DJANGO_ADMIN_FORCE_ALLAUTH:
     # Force the `admin` sign in process to go through the `django-allauth` workflow:
@@ -38,3 +38,30 @@ class UserAdmin(auth_admin.UserAdmin):
     )
     list_display = ["username", "name", "is_superuser"]
     search_fields = ["name"]
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    """Admin panel for customers."""
+
+    list_display = ["id", "user", "date_of_birth"]
+    search_fields = ["user__username", "user__email"]
+    ordering = ["id"]
+
+
+@admin.register(Seller)
+class SellerAdmin(admin.ModelAdmin):
+    """Admin panel for sellers."""
+
+    list_display = ["id", "user", "shop_name", "shop_description"]
+    search_fields = ["user__username", "shop_name"]
+    ordering = ["id"]
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    """Admin panel for addresses."""
+
+    list_display = ["id", "user", "street", "city", "country"]
+    search_fields = ["user__username", "street", "city", "country"]
+    ordering = ["id"]
